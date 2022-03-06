a4be65b8dd013942324b8e9656f55dbaa77666be
tomgalbar <72520181+tomgalbar@users.noreply.github.com>
2022-03-06 18:10:27 +0100

    Fix Lint error

--- controllers/AuthControllerService.js
+++ controllers/AuthControllerService.js
@@ -3,7 +3,7 @@
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgresql://stackingup:stackingup-local@localhost:5432/data';
 
-const pool = new Pool({connectionString});
+const pool = new Pool({ connectionString });
 
 module.exports.login = function login (req, res, next) {
   res.send({
