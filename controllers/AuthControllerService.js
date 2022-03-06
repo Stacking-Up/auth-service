21d686ead2a26e93d59f52e1d3a8712eb50b95e8
tomgalbar <tomgalbar@alum.us.es>
2022-03-06 17:32:56 +0100

    feat: Added auth DB connection

--- controllers/AuthControllerService.js
+++ controllers/AuthControllerService.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgresql://stackingup:stackingup-local@localhost:5432/data';
+
+const pool = new Pool({connectionString});
+
 module.exports.login = function login (req, res, next) {
   res.send({
     message: 'This is the mockup controller for login'
