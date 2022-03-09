ee272111a3af04a789f39084997665df631dbbbd
alesancor1 <alesancor1@alum.us.es>
2022-03-09 04:01:34 +0100

    test: Implemented some tests

--- controllers/AuthControllerService.js
+++ controllers/AuthControllerService.js
@@ -2,30 +2,29 @@
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-
-const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgresql://stackingup:stackingup-local@localhost:5432/data';
-const pool = new Pool({ connectionString });
+const pool = require('../utils/dbCon');
 
 module.exports.login = function login (req, res, next) {
   const { username, password } = req.credentials.value;
   const secret = process.env.JWT_SECRET || 'stackingupsecretlocal';
-
-  pool.query('SELECT * FROM "Auth" WHERE "email" = $1', [username], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error trying to connect to the database');
-    }
+  
+  pool.query('SELECT * FROM "Auth" WHERE "email" = $1', [username]).then(result => {
 
     if (result.rows.length === 0 || !bcrypt.compareSync(password, result.rows[0].password)) {
-      return res.status(400).send('Invalid username or password');
-    } else {
-      const token = jwt.sign({
-        email: result.rows[0].email,
-        role: result.rows[0].role,
-        userId: result.rows[0].userId
-      }, secret, { expiresIn: '1h' });
-      return res.status(200).send(token);
-    }
+      res.status(400).send('Invalid username or password');
+      return;
+    } 
+
+    const token = jwt.sign({
+      email: result.rows[0].email,
+      role: result.rows[0].role,
+      userId: result.rows[0].userId
+    }, secret, { expiresIn: '1h' });
+
+    res.status(200).send(token);
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).send('Internal server error');
   });
 };
